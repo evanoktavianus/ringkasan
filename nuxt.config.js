88883c488b6726913367e601953bb70ba47b203d
Litera Institute <literakmsd@gmail.com>
2020-03-11 08:22:01 +0700

    nuxt config added for firebaseConfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,61 @@
-
 export default {
-  server:{port:8000,host:'0.0.0.0'},
-  mode: 'universal',
+  env: {
+    firebaseConfig: {
+      apiKey: "AIzaSyA1get978WjnEvb2k7JElWsPQ4AmsRBTfk",
+      authDomain: "vue-project-9e3e6.firebaseapp.com",
+      databaseURL: "https://vue-project-9e3e6.firebaseio.com",
+      projectId: "vue-project-9e3e6",
+      storageBucket: "vue-project-9e3e6.appspot.com",
+      messagingSenderId: "495962986000",
+      appId: "1:495962986000:web:d9f9fdd6498a9a319d9c95",
+      measurementId: "G-NNJ79K0XBG"
+    }
+  },
+  server: { port: 8000, host: "0.0.0.0" },
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
